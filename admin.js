c5d7878a3af9f85365808f7be8d1745095d0ed3c
dearnadiya <euishunadiyah@gmail.com>
2026-09-04 14:16:41 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -8420,6 +8420,40 @@ function formatDateTime(
 }
 
 /* ============================================
+   FORMAT TANGGAL PO
+   ============================================ */
+
+function formatDateTime(value) {
+
+  if (!value) {
+    return "—";
+  }
+
+  const date =
+    new Date(value);
+
+  if (
+    Number.isNaN(
+      date.getTime()
+    )
+  ) {
+    return "—";
+  }
+
+  return date.toLocaleString(
+    "id-ID",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit"
+    }
+  );
+
+}
+
+/* ============================================
    BAGIAN 6
    FINAL INITIALIZATION
    ============================================ */
